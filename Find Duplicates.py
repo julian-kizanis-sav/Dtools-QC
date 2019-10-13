7918fedb1863eedd68f7a51393ea82516d641af2
julian-kizanis-sav <55302122+julian-kizanis-sav@users.noreply.github.com>
2019-10-13 14:47:03 -0600

    Add files via upload

--- Find Duplicates.py
+++ Find Duplicates.py
@@ -1,58 +1,59 @@
 #SAV Digital Environments
 #Julian Kizanis
+#ETA = 1hour
 print("Commpany:\tSAV Digital Environments\nDeveloper:\tJulian Kizanis\n\
 Powered By:\tAnaconda\n\n")
 
 import pandas as pd
 from fuzzywuzzy import fuzz
 #import numpy as np
+#import math
 
-dt_all = pd.read_csv("Models.csv")	#imports the csv file into a dataframe
 
-def Dup(dup, dup_index, level):
+def Dup(dup, index_1, level):
 	if level == 0:
-		dup.loc[dup_index, 'Duplicate[2]'] = dup.loc[dup_index, 'Duplicate[1]']
-		dup.loc[dup_index, 'Duplicate[1]'] = dup.loc[dup_index, 'Duplicate[0]']
-		dup.loc[dup_index, 'Duplicate[0]'] = model_2
+		dup.loc[index_1, 'Duplicate[2]'] = dup.loc[index_1, 'Duplicate[1]']
+		dup.loc[index_1, 'Duplicate[1]'] = dup.loc[index_1, 'Duplicate[0]']
+		dup.loc[index_1, 'Duplicate[0]'] = model_2
 		
-		dup.loc[dup_index, 'Partial Match Ratio[2]'] = dup.loc[dup_index, 'Partial Match Ratio[1]']
-		dup.loc[dup_index, 'Partial Match Ratio[1]'] = dup.loc[dup_index, 'Partial Match Ratio[0]']
-		dup.loc[dup_index, 'Partial Match Ratio[0]'] = temp_partial_match_ratio
+		dup.loc[index_1, 'Partial Match Ratio[2]'] = dup.loc[index_1, 'Partial Match Ratio[1]']
+		dup.loc[index_1, 'Partial Match Ratio[1]'] = dup.loc[index_1, 'Partial Match Ratio[0]']
+		dup.loc[index_1, 'Partial Match Ratio[0]'] = temp_partial_match_ratio
 		
-		dup.loc[dup_index, 'Match Ratio[2]'] = dup.loc[dup_index, 'Match Ratio[1]']
-		dup.loc[dup_index, 'Match Ratio[1]'] = dup.loc[dup_index, 'Match Ratio[0]']
-		dup.loc[dup_index, 'Match Ratio[0]'] = temp_match_ratio
+		dup.loc[index_1, 'Match Ratio[2]'] = dup.loc[index_1, 'Match Ratio[1]']
+		dup.loc[index_1, 'Match Ratio[1]'] = dup.loc[index_1, 'Match Ratio[0]']
+		dup.loc[index_1, 'Match Ratio[0]'] = temp_match_ratio
 		
-		dup.loc[dup_index, 'Index[2]'] = dup.loc[dup_index, 'Index[1]']
-		dup.loc[dup_index, 'Index[1]'] = dup.loc[dup_index, 'Index[0]']
-		dup.loc[dup_index, 'Index[0]'] = index_2
+		dup.loc[index_1, 'Index[2]'] = dup.loc[index_1, 'Index[1]']
+		dup.loc[index_1, 'Index[1]'] = dup.loc[index_1, 'Index[0]']
+		dup.loc[index_1, 'Index[0]'] = index_2
 		
 	elif level == 1:
-		dup.loc[dup_index, 'Duplicate[2]'] = dup.loc[dup_index, 'Duplicate[1]']
-		dup.loc[dup_index, 'Duplicate[1]'] = model_2
+		dup.loc[index_1, 'Duplicate[2]'] = dup.loc[index_1, 'Duplicate[1]']
+		dup.loc[index_1, 'Duplicate[1]'] = model_2
 		
-		dup.loc[dup_index, 'Partial Match Ratio[2]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[1]']
-		dup.loc[dup_index, 'Partial Match Ratio[1]'] = temp_partial_match_ratio
+		dup.loc[index_1, 'Partial Match Ratio[2]'] = dup.loc[index_1, 'Partial Match Ratio[1]']
+		dup.loc[index_1, 'Partial Match Ratio[1]'] = temp_partial_match_ratio
 		
-		dup.loc[dup_index, 'Match Ratio[2]'] = dup.loc[dup_index, 'Match Ratio[1]']
-		dup.loc[dup_index, 'Match Ratio[1]'] = temp_match_ratio
+		dup.loc[index_1, 'Match Ratio[2]'] = dup.loc[index_1, 'Match Ratio[1]']
+		dup.loc[index_1, 'Match Ratio[1]'] = temp_match_ratio
 		
-		dup.loc[dup_index, 'Index[2]'] = dup.loc[dup_index, 'Index[1]']
-		dup.loc[dup_index, 'Index[1]'] = index_2	
+		dup.loc[index_1, 'Index[2]'] = dup.loc[index_1, 'Index[1]']
+		dup.loc[index_1, 'Index[1]'] = index_2	
 		
 	elif level == 2:
-		dup.loc[dup_index, 'Duplicate[2]'] = model_2
+		dup.loc[index_1, 'Duplicate[2]'] = model_2
 		
-		dup.loc[dup_index, 'Partial Match Ratio[2]'] = temp_partial_match_ratio
+		dup.loc[index_1, 'Partial Match Ratio[2]'] = temp_partial_match_ratio
 		
-		dup.loc[dup_index, 'Match Ratio[2]'] = temp_match_ratio
+		dup.loc[index_1, 'Match Ratio[2]'] = temp_match_ratio
 		
-		dup.loc[dup_index, 'Index[2]'] = index_2
+		dup.loc[index_1, 'Index[2]'] = index_2
 		
 	return dup
 
-
-Duplicates = pd.DataFrame(columns = {'Model Index',	'Model Number',\
+def Dup_ini():
+	dup = pd.DataFrame(columns = {'Model Index',	'Model Number',\
 									 'Best Match Ratio',\
 									 'Duplicate[0]', 'Match Ratio[0]',\
 									 'Partial Match Ratio[0]','Index[0]',\
@@ -60,12 +61,19 @@ Duplicates = pd.DataFrame(columns = {'Model Index',	'Model Number',\
 									 'Partial Match Ratio[1]','Index[1]',\
 									 'Duplicate[2]','Match Ratio[2]',\
 									 'Partial Match Ratio[2]','Index[2]'})
+	return dup[['Model Index',	'Model Number',\
+				 'Best Match Ratio',\
+				 'Duplicate[0]', 'Match Ratio[0]',\
+				 'Partial Match Ratio[0]','Index[0]',\
+				 'Duplicate[1]','Match Ratio[1]',\
+				 'Partial Match Ratio[1]','Index[1]',\
+				 'Duplicate[2]','Match Ratio[2]',\
+				 'Partial Match Ratio[2]','Index[2]']]
 	
-
-#dup_index = 0
-for index_1, model_1 in enumerate(dt_all.loc[:,"Model"]):
-	dup_index = index_1
-	Duplicates = Duplicates.append({'Model Index':index_1,\
+	
+	
+def Dup_new_row(dup):
+	return dup.append({'Model Index':index_1,\
 					 'Model Number':model_1,'Best Match Ratio':0,\
 					 'Duplicate[0]':'', 'Match Ratio[0]':0,\
 					 'Partial Match Ratio[0]':0,'Index[0]':0,\
@@ -74,92 +82,116 @@ for index_1, model_1 in enumerate(dt_all.loc[:,"Model"]):
 					 'Duplicate[2]':'','Match Ratio[2]':0,\
 					 'Partial Match Ratio[2]':0,'Index[2]':0},\
 						ignore_index=True)
-	
+
+#Duplicates_Hybrid.loc[index_1, 'Best Match Ratio'] = hmr_h
+def insertion_sort(dup):
+        
+    for i in range(len(dup.loc[:,'Best Match Ratio'])):
+        cursor = dup.loc[i,'Best Match Ratio']
+        pos = i
+        
+        while pos > 0 and dup.loc[pos - 1,'Best Match Ratio'] > cursor:
+            # Swap the number down the list
+            dup.loc[pos,:] = dup.loc[pos - 1,:]
+            pos = pos - 1
+        # Break and do the final swap
+        dup.loc[pos,:] = dup.loc[i,:]
+
+    return dup
+
+
+dt_all = pd.read_csv("Models.csv")	#imports the csv file into a dataframe	
+
+print("('m':Match, 'p':partial, 'h':hybrid, 'a':all)")
+match_type = input("Enter Match Type: ")
+
+if match_type == 'm' or match_type == 'a':
+	Duplicates_Match = Dup_ini()
+if match_type == 'p' or match_type == 'a':
+	Duplicates_Partial = Dup_ini()
+if match_type == 'h' or match_type == 'a':
+	Duplicates_Hybrid = Dup_ini()	
+
+
+
+#index_1 = 0
+for index_1, model_1 in enumerate(dt_all.loc[:,"Model"]):
+	if match_type == 'p' or match_type == 'a':
+		Duplicates_Partial = Dup_new_row(Duplicates_Partial)
+	if match_type == 'm' or match_type == 'a':
+		Duplicates_Match = Dup_new_row(Duplicates_Match)
+	if match_type == 'h' or match_type == 'a':
+		Duplicates_Hybrid = Dup_new_row(Duplicates_Hybrid)
 	
 	for index_2, model_2 in enumerate(dt_all.loc[:,"Model"]):
 		if index_1 != index_2:
 			temp_partial_match_ratio = fuzz.partial_ratio(model_1.lower(), model_2.lower())
 			temp_match_ratio = fuzz.ratio(model_1.lower(), model_2.lower())
-
-			if temp_partial_match_ratio > Duplicates.loc[dup_index, 'Partial Match Ratio[0]']:
-				Duplicates.loc[dup_index, 'Duplicate[2]'] = Duplicates.loc[dup_index, 'Duplicate[1]']
-				Duplicates.loc[dup_index, 'Duplicate[1]'] = Duplicates.loc[dup_index, 'Duplicate[0]']
-				Duplicates.loc[dup_index, 'Duplicate[0]'] = model_2
+			if match_type == 'p' or match_type == 'a':
+				if temp_partial_match_ratio > Duplicates_Partial.loc[index_1, 'Partial Match Ratio[0]']:
+					Duplicates_Partial = Dup(Duplicates_Partial, index_1, 0)
+					
+				elif temp_partial_match_ratio == Duplicates_Partial.loc[index_1, 'Partial Match Ratio[0]']\
+				and temp_match_ratio > Duplicates_Partial.loc[index_1, 'Match Ratio[0]']:
+					Duplicates_Partial = Dup(Duplicates_Partial, index_1, 0)
+					
+				elif temp_partial_match_ratio > Duplicates_Partial.loc[index_1, 'Partial Match Ratio[1]']:
+					Duplicates_Partial = Dup(Duplicates_Partial, index_1, 1)
+					
+				elif temp_partial_match_ratio == Duplicates_Partial.loc[index_1, 'Partial Match Ratio[1]']\
+				and temp_match_ratio > Duplicates_Partial.loc[index_1, 'Match Ratio[1]']:
+					Duplicates_Partial = Dup(Duplicates_Partial, index_1, 1)						
+					
+				elif temp_partial_match_ratio > Duplicates_Partial.loc[index_1, 'Partial Match Ratio[2]']:
+					Duplicates_Partial = Dup(Duplicates_Partial, index_1, 2)
 				
-				Duplicates.loc[dup_index, 'Partial Match Ratio[2]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[1]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[0]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[0]'] = temp_partial_match_ratio
-				
-				Duplicates.loc[dup_index, 'Match Ratio[2]'] = Duplicates.loc[dup_index, 'Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Match Ratio[1]'] = Duplicates.loc[dup_index, 'Match Ratio[0]']
-				Duplicates.loc[dup_index, 'Match Ratio[0]'] = temp_match_ratio
-				
-				Duplicates.loc[dup_index, 'Index[2]'] = Duplicates.loc[dup_index, 'Index[1]']
-				Duplicates.loc[dup_index, 'Index[1]'] = Duplicates.loc[dup_index, 'Index[0]']
-				Duplicates.loc[dup_index, 'Index[0]'] = index_2
-				
-			elif temp_partial_match_ratio == Duplicates.loc[dup_index, 'Partial Match Ratio[0]']\
-			and temp_match_ratio > Duplicates.loc[dup_index, 'Match Ratio[0]']:
-				Duplicates.loc[dup_index, 'Duplicate[2]'] = Duplicates.loc[dup_index, 'Duplicate[1]']
-				Duplicates.loc[dup_index, 'Duplicate[1]'] = Duplicates.loc[dup_index, 'Duplicate[0]']
-				Duplicates.loc[dup_index, 'Duplicate[0]'] = model_2
-				
-				Duplicates.loc[dup_index, 'Partial Match Ratio[2]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[1]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[0]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[0]'] = temp_partial_match_ratio
-				
-				Duplicates.loc[dup_index, 'Match Ratio[2]'] = Duplicates.loc[dup_index, 'Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Match Ratio[1]'] = Duplicates.loc[dup_index, 'Match Ratio[0]']
-				Duplicates.loc[dup_index, 'Match Ratio[0]'] = temp_match_ratio
-				
-				Duplicates.loc[dup_index, 'Index[2]'] = Duplicates.loc[dup_index, 'Index[1]']
-				Duplicates.loc[dup_index, 'Index[1]'] = Duplicates.loc[dup_index, 'Index[0]']
-				Duplicates.loc[dup_index, 'Index[0]'] = index_2
-				
-			elif temp_partial_match_ratio > Duplicates.loc[dup_index, 'Partial Match Ratio[1]']:
-				Duplicates.loc[dup_index, 'Duplicate[2]'] = Duplicates.loc[dup_index, 'Duplicate[1]']
-				Duplicates.loc[dup_index, 'Duplicate[1]'] = model_2
-				
-				Duplicates.loc[dup_index, 'Partial Match Ratio[2]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[1]'] = temp_partial_match_ratio
-				
-				Duplicates.loc[dup_index, 'Match Ratio[2]'] = Duplicates.loc[dup_index, 'Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Match Ratio[1]'] = temp_match_ratio
-				
-				Duplicates.loc[dup_index, 'Index[2]'] = Duplicates.loc[dup_index, 'Index[1]']
-				Duplicates.loc[dup_index, 'Index[1]'] = index_2			
-				
-			elif temp_partial_match_ratio == Duplicates.loc[dup_index, 'Partial Match Ratio[1]']\
-			and temp_match_ratio > Duplicates.loc[dup_index, 'Match Ratio[1]']:
-				Duplicates.loc[dup_index, 'Duplicate[2]'] = Duplicates.loc[dup_index, 'Duplicate[1]']
-				Duplicates.loc[dup_index, 'Duplicate[1]'] = model_2
-				
-				Duplicates.loc[dup_index, 'Partial Match Ratio[2]'] = Duplicates.loc[dup_index, 'Partial Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Partial Match Ratio[1]'] = temp_partial_match_ratio
-				
-				Duplicates.loc[dup_index, 'Match Ratio[2]'] = Duplicates.loc[dup_index, 'Match Ratio[1]']
-				Duplicates.loc[dup_index, 'Match Ratio[1]'] = temp_match_ratio
-				
-				Duplicates.loc[dup_index, 'Index[2]'] = Duplicates.loc[dup_index, 'Index[1]']
-				Duplicates.loc[dup_index, 'Index[1]'] = index_2							
-				
-			elif temp_partial_match_ratio > Duplicates.loc[dup_index, 'Partial Match Ratio[1]']:
-				Duplicates.loc[dup_index, 'Duplicate[2]'] = model_2
-				
-				Duplicates.loc[dup_index, 'Partial Match Ratio[2]'] = temp_partial_match_ratio
-				
-				Duplicates.loc[dup_index, 'Match Ratio[2]'] = temp_match_ratio
-				
-				Duplicates.loc[dup_index, 'Index[2]'] = index_2
-							
-	print(f"{model_1}:\t{Duplicates.loc[dup_index, 'Duplicate[0]']}\
-   \tParMatch% = {Duplicates.loc[dup_index, 'Partial Match Ratio[0]']}%\
-   Match% = {Duplicates.loc[dup_index, 'Match Ratio[0]']}%")			
-				
-				
-				
-				
-				
+			if match_type == 'm' or match_type == 'a':				
+				if temp_match_ratio > Duplicates_Match.loc[index_1, 'Match Ratio[0]']:
+					Duplicates_Match = Dup(Duplicates_Match, index_1, 0)			
+					
+				elif temp_match_ratio > Duplicates_Match.loc[index_1, 'Match Ratio[1]']:
+					Duplicates_Match = Dup(Duplicates_Match, index_1, 1)
+					
+				elif temp_match_ratio > Duplicates_Match.loc[index_1, 'Match Ratio[2]']:
+					Duplicates_Match = Dup(Duplicates_Match, index_1, 2)
+	
+			if match_type == 'h' or match_type == 'a':	
+				if temp_match_ratio * temp_partial_match_ratio > \
+				Duplicates_Hybrid.loc[index_1, 'Match Ratio[0]'] * Duplicates_Hybrid.loc[index_1, 'Partial Match Ratio[0]']:
+					Duplicates_Hybrid = Dup(Duplicates_Hybrid, index_1, 0)			
+					
+				elif temp_match_ratio * temp_partial_match_ratio > \
+				Duplicates_Hybrid.loc[index_1, 'Match Ratio[1]'] * Duplicates_Hybrid.loc[index_1, 'Partial Match Ratio[1]']:
+					Duplicates_Hybrid = Dup(Duplicates_Hybrid, index_1, 1)
+					
+				elif temp_match_ratio * temp_partial_match_ratio > \
+				Duplicates_Hybrid.loc[index_1, 'Match Ratio[2]'] * Duplicates_Hybrid.loc[index_1, 'Partial Match Ratio[2]']:
+					Duplicates_Hybrid = Dup(Duplicates_Hybrid, index_1, 2)
+#	hmr = math.sqrt(Duplicates_Partial.loc[index_1, 'Match Ratio[0]'] * Duplicates_Partial.loc[index_1, 'Partial Match Ratio[0]'])					
+#	print(f"{model_1}:\tHybrid Match % = {hmr}%")			
+	
+	
+						
+	if match_type == 'h' or match_type == 'a':	
+		hmr_h = (Duplicates_Hybrid.loc[index_1, 'Match Ratio[0]'] * Duplicates_Hybrid.loc[index_1, 'Partial Match Ratio[0]'])/100	
+		Duplicates_Hybrid.loc[index_1, 'Best Match Ratio'] = hmr_h
+		Duplicates_Hybrid = Duplicates_Hybrid.sort_values('Best Match Ratio',ascending=False)
+		print(f"{model_1}\t\t{Duplicates_Hybrid.loc[index_1, 'Duplicate[0]']}\t{hmr_h}")	
+		export_csv = Duplicates_Hybrid.to_csv ('Duplicates_Hybrid.csv', header=True)
+		
+	if match_type == 'm' or match_type == 'a':				
+		hmr_m = (Duplicates_Match.loc[index_1, 'Match Ratio[0]'] * Duplicates_Match.loc[index_1, 'Partial Match Ratio[0]'])/100
+		Duplicates_Match.loc[index_1, 'Best Match Ratio'] = hmr_m
+		insertion_sort(Duplicates_Match)
+		print(f"{model_1}\t\t{Duplicates_Match.loc[index_1, 'Duplicate[0]']}\t{hmr_m}")	
+		export_csv = Duplicates_Match.to_csv ('Duplicates_Match.csv', header=True)
+		
+	if match_type == 'p' or match_type == 'a':
+		hmr_p = (Duplicates_Partial.loc[index_1, 'Match Ratio[0]'] * Duplicates_Partial.loc[index_1, 'Partial Match Ratio[0]'])/100
+		Duplicates_Partial.loc[index_1, 'Best Match Ratio'] = hmr_p
+		insertion_sort(Duplicates_Partial)				
+		print(f"{model_1}\t\t{Duplicates_Partial.loc[index_1, 'Duplicate[0]']}\t{hmr_p}")
+		export_csv = Duplicates_Partial.to_csv ('Duplicates_Partial.csv', header=True)		
 				
 				
 				
